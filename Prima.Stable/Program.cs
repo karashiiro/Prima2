8a53fd4032d42f514526b3c1d16bebb8274913ea
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-02-03 09:08:21 -0800

    Restriction handler for twitch streams

--- Prima.Stable/Program.cs
+++ Prima.Stable/Program.cs
@@ -36,6 +36,7 @@ namespace Prima.Stable
 
             client.MessageReceived += message => MessageCache.Handler(db, message);
             client.MessageReceived += message => ExtraMessageReceived.Handler(client, message);
+            client.MessageReceived += DRSStream.Handler;
 
             client.GuildMemberUpdated += censusEvents.GuildMemberUpdated;
 
