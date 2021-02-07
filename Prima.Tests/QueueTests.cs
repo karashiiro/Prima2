6a058674aa22742fe00d6b27fda6ccfc3f8f2150
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-02-06 17:11:03 -0800

    Diversify tests

--- Prima.Tests/QueueTests.cs
+++ Prima.Tests/QueueTests.cs
@@ -6,11 +6,11 @@ namespace Prima.Tests
     public class QueueTests
     {
         [Test]
-        [TestCase("1d1h1t", 1, 1, 1)]
+        [TestCase("1d3h1t", 1, 3, 1)]
         [TestCase("1d1h", 1, 1, 0)]
         [TestCase("1d", 1, 0, 0)]
-        [TestCase("1d 1healer 1 tank", 1, 1, 1)]
-        [TestCase("1h1t1d", 1, 1, 1)]
+        [TestCase("1d 4healer 2 tank", 1, 4, 2)]
+        [TestCase("4h1t1d", 1, 4, 1)]
         [TestCase("11h11t11d", 11, 11, 11)]
         [TestCase("8h88t1d", 1, 8, 88)]
         public void GetDesiredRoleCounts_WorksAsExpected(string input, int dExpected, int hExpected, int tExpected)
