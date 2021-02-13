22e79caecbb4c87206da99693356894e267b7b0b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-02-12 17:59:16 -0800

    More bugfixes

--- Prima.Scheduler/Handlers/AnnounceReact.cs
+++ Prima.Scheduler/Handlers/AnnounceReact.cs
@@ -9,30 +9,32 @@ namespace Prima.Scheduler.Handlers
 {
     public static class AnnounceReact
     {
-        public static async Task HandlerAdd(DiscordSocketClient client, DbService db, Cacheable<IUserMessage, ulong> cachedMessage, ulong userId)
+        public static async Task HandlerAdd(DiscordSocketClient client, DbService db, Cacheable<IUserMessage, ulong> cachedMessage, SocketReaction reaction)
         {
-            if (client.CurrentUser.Id == userId) return;
+            var userId = reaction.UserId;
+            if (client.CurrentUser.Id == userId || reaction.Emote.Name != "📳") return;
 
             var eventId = await GetEventId(cachedMessage);
             if (eventId == null) return;
             if (await db.AddEventReaction(eventId.Value, userId))
             {
                 var user = client.GetUser(userId);
+                await user.SendMessageAsync($"You have signed up for notifications for run {eventId}!");
                 Log.Information("User {User} has signed up for notifications for event {EventId}.", user.ToString(), eventId);
             }
-        }
-
-        public static async Task HandlerRemove(DiscordSocketClient client, DbService db, Cacheable<IUserMessage, ulong> cachedMessage, ulong userId)
-        {
-            if (client.CurrentUser.Id == userId) return;
-
-            var eventId = await GetEventId(cachedMessage);
-            if (eventId == null) return;
-            if (await db.RemoveEventReaction(eventId.Value, userId))
+            else if (await db.RemoveEventReaction(eventId.Value, userId))
             {
                 var user = client.GetUser(userId);
+                await user.SendMessageAsync($"You have been removed from the notifications list for event {eventId}.");
                 Log.Information("User {User} has been removed from notifications for event {EventId}.", user.ToString(), eventId);
             }
+
+            var message = await cachedMessage.GetOrDownloadAsync();
+            try
+            {
+                await message.RemoveReactionAsync(new Emoji("📳"), userId);
+            }
+            catch { /* ignored */ }
         }
 
         private static async Task<ulong?> GetEventId(Cacheable<IUserMessage, ulong> cachedMessage)
--- Prima.Scheduler/Program.cs
+++ Prima.Scheduler/Program.cs
@@ -32,9 +32,7 @@ namespace Prima.Scheduler
 
             client.MessageUpdated += (_, message, channel) => AnnounceEdit.Handler(client, calendar, db, message);
             client.ReactionAdded += (cachedMessage, channel, reaction)
-                => AnnounceReact.HandlerAdd(client, db, cachedMessage, reaction.UserId);
-            client.ReactionRemoved += (cachedMessage, channel, reaction)
-                => AnnounceReact.HandlerRemove(client, db, cachedMessage, reaction.UserId);
+                => AnnounceReact.HandlerAdd(client, db, cachedMessage, reaction);
 
             services.GetRequiredService<RunNotiferService>().Initialize();
             services.GetRequiredService<AnnounceMonitor>().Initialize();
