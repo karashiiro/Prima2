1ee8b6afe8df6683634d262fa479d16d02628b42
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-02-23 11:46:41 -0800

    Add TimedEvent and TimedEvent tests

---
+++ Prima.Tests/TimedEventTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Prima.Tests
+{
+    [TestFixture]
+    public class TimedEventTests
+    {
+        [Test]
+        public async Task GetResult_CanBeRunInParallel()
+        {
+            var count = 0;
+            for (var i = 0; i < 10; i++)
+            {
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+                var task1 = new TimedEvent(1, 0.05,
+                    () => Task.FromResult(false)).GetResult();
+                var task2 = new TimedEvent(1, 0.05,
+                    () => Task.FromResult(false)).GetResult();
+                await Task.WhenAll(task1, task2);
+                stopwatch.Stop();
+
+                if (stopwatch.ElapsedMilliseconds < 1150)
+                    count++;
+            }
+
+            Assert.That(count > 7);
+        }
+
+        [Test]
+        public async Task GetResult_ReturnsFalseOnFailure()
+        {
+            var result = await new TimedEvent(1, 0.05,
+                () => Task.FromResult(false)).GetResult();
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public async Task GetResult_ReturnsTrueOnSuccess()
+        {
+            var result = await new TimedEvent(1, 0.05,
+                () => Task.FromResult(true)).GetResult();
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public async Task GetResult_DoesNotReturnEarly()
+        {
+            var once = false;
+            await new TimedEvent(1, 0.05, () =>
+            {
+                // ReSharper disable once InvertIf
+                if (!once)
+                {
+                    once = true;
+                    return Task.FromResult(false);
+                }
+
+                return Task.FromResult(true);
+            }).GetResult();
+            Assert.IsTrue(once);
+        }
+    }
+}
---
+++ Prima/TimedEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Prima
+{
+    public class TimedEvent
+    {
+        private readonly double _timeoutSeconds;
+        private readonly int _checkIntervalMs;
+        private readonly Func<Task<bool>> _query;
+
+        public TimedEvent(double timeoutSeconds, double checkIntervalSeconds, Func<Task<bool>> query)
+        {
+            _timeoutSeconds = timeoutSeconds;
+            _checkIntervalMs = (int)(checkIntervalSeconds * 1000);
+            _query = query;
+        }
+
+        public async Task<bool> GetResult()
+        {
+            var endTime = DateTime.UtcNow.AddSeconds(_timeoutSeconds);
+            while (DateTime.UtcNow < endTime)
+            {
+                if (await _query())
+                {
+                    return true;
+                }
+
+                await Task.Delay(_checkIntervalMs);
+            }
+
+            return false;
+        }
+    }
+}
