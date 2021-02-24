1bf41f03dfe42f02d2eef8e0ae18586ace3df615
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-02-24 00:14:28 -0800

    Steal a gQL query for FFLogs to get the players in a fight in a log

---
+++ Prima.Stable/Resources/FFLogs.cs
@@ -0,0 +1,30 @@
+﻿namespace Prima.Stable.Resources
+{
+    public static class FFLogs
+    {
+        public static string BuildMembersRequest(string logId, int fightId)
+        {
+            return $@"
+            {{
+                reportData: {{
+                    report(code: ""{logId}"") {{
+                        fights(fightIDs: [{fightId}]) {{
+                            encounterID,
+                            kill,
+                            endTime,
+                            startTime,
+                            name,
+                            friendlyPlayers
+                        }},
+                        masterData {{
+                            actors {{
+                                name,
+                                server
+                            }}
+                        }}
+                    }}
+                }}
+            }}";
+        }
+    }
+}
