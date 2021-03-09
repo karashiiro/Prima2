08c3cda434393e66a2cb3dce0240b8e1b4496fd6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-08 19:03:04 -0800

    ↖️ ↙️ ↘️ ↗️

--- Prima.Stable/Handlers/ExtraMessageReceived.cs
+++ Prima.Stable/Handlers/ExtraMessageReceived.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Discord;
 using Discord.WebSocket;
 using Prima.Resources;
 
@@ -8,17 +9,28 @@ namespace Prima.Stable.Handlers
     {
         public static async Task Handler(DiscordSocketClient client, SocketMessage message)
         {
+            var cem = client.GetGuild(SpecialGuilds.CrystalExploratoryMissions);
+            var isCEMChannel = cem.GetTextChannel(message.Channel.Id) != null;
+
             if (message.Content == "(╯°□°）╯︵ ┻━┻")
             {
                 await message.Channel.SendMessageAsync("┬─┬ ノ( ゜-゜ノ)");
                 return;
             }
 
-            if (HasWord(message.Content, "sch") || HasWord(message.Content, "scholar"))
+            if (isCEMChannel && (HasWord(message.Content, "sch") || HasWord(message.Content, "scholar")))
             {
-                var guild = client.GetGuild(SpecialGuilds.CrystalExploratoryMissions);
-                var emote = await guild.GetEmoteAsync(573531927613800459); // SCH emote
+                var emote = await cem.GetEmoteAsync(573531927613800459); // SCH emote
                 await message.AddReactionAsync(emote);
+            }
+
+            if (isCEMChannel && (HasWord(message.Content, "intercardinals") || HasWord(message.Content, "intercards") || message.Content.Contains("<@383805961216983061>")))
+            {
+                var emotes = new[] {new Emoji("↖️"), new Emoji("↙️"), new Emoji("↘️"), new Emoji("↗️") };
+                foreach (var emote in emotes)
+                {
+                    await message.AddReactionAsync(emote);
+                }
             }
         }
 
