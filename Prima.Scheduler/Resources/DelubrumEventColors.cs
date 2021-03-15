05d76632204eed10f8d251bfeccc44c8e78c0bc8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-03-14 19:33:04 -0700

    Stub out colors for DRS events

---
+++ Prima.Scheduler/Resources/DelubrumEventColors.cs
@@ -0,0 +1,12 @@
+﻿namespace Prima.Scheduler.Resources
+{
+    public enum DelubrumEventColors
+    {
+        FreshProg = 1,
+        TrinitySeeker = 3,
+        QueensGuard = 4,
+        TrinityAvowed = 7,
+        StygimolochLord = 9,
+        TheQueen = 11,
+    }
+}
