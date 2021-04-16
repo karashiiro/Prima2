b7eb60c7e9454f08ab6ee20dda88d59926a0adeb
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-15 21:09:49 -0700

    Add ~lfgcountsba

--- Prima.Stable/Modules/BAExtraModule.cs
+++ Prima.Stable/Modules/BAExtraModule.cs
@@ -43,6 +43,31 @@ namespace Prima.Stable.Modules
             await ReplyAsync(embed: embed);
         }
 
+        [Command("lfgcountsba", RunMode = RunMode.Async)]
+        [Description("Get the LFG role counts of all guild members for the Baldesion Arsenal.")]
+        [RestrictToGuilds(SpecialGuilds.CrystalExploratoryMissions)]
+        public Task GetBAProgressionCounts()
+        {
+            var members = Context.Guild.Users;
+
+            var lfgFragMembers = members.Where(m => m.HasRole(551619522340323328));
+            var lfgAvOzmaMembers = members.Where(m => m.HasRole(551619527008321557));
+            var lfgOzmaClearMembers = members.Where(m => m.HasRole(551619529336422402));
+            var lfgBaAllMembers = members.Where(m => m.HasRole(574696475976794175));
+
+            const string outFormat = "LFG Counts (includes overlap):\n" +
+                                     "LFG Frags/Fresh Prog: {0}\n" +
+                                     "LFG AV/Ozma Prog: {1}\n" +
+                                     "LFG Ozma Clears/Farms: {2}\n" +
+                                     "LFG BA All: {3}";
+            return ReplyAsync(string.Format(
+                outFormat,
+                lfgFragMembers.Count(),
+                lfgAvOzmaMembers.Count(),
+                lfgOzmaClearMembers.Count(),
+                lfgBaAllMembers.Count()));
+        }
+
         [Command("portals", RunMode = RunMode.Async)]
         [Description("Shows the portal map.")]
         [RateLimit(TimeSeconds = 30, Global = true)]
