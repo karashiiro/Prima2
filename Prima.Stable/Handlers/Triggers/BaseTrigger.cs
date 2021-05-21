5663a110d29cefefbedf242937f7a193255dd9ff
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-21 01:50:12 -0700

    Add better documentation to new BaseTrigger

--- Prima.Stable/Handlers/Triggers/BaseTrigger.cs
+++ Prima.Stable/Handlers/Triggers/BaseTrigger.cs
@@ -18,22 +18,34 @@ namespace Prima.Stable.Handlers.Triggers
             _guildId = guildId;
         }
 
+        /// <summary>
+        /// Returns true if the conditions for the trigger are met.
+        /// </summary>
         public abstract bool Condition(SocketMessage message);
 
+        /// <summary>
+        /// Runs the message trigger.
+        /// </summary>
         public abstract Task Execute(DiscordSocketClient client, SocketMessage message);
 
+        /// <summary>
+        /// Returns the applicable guild ID for this trigger. A value of 0 means it can be used anywhere.
+        /// </summary>
         public ulong GetApplicableGuildId()
         {
             return _guildId;
         }
 
+        /// <summary>
+        /// Returns true if the provided guild is supported by this trigger.
+        /// </summary>
         public bool IsGuildApplicable(SocketGuild guild)
         {
             return _guildId == 0 || _guildId == guild?.Id;
         }
 
         /// <summary>
-        /// Helper method for detecting if a phrase contains a certain word.
+        /// Returns true if the provided phrase contains the provided word.
         /// </summary>
         protected static bool HasWord(string phrase, string word)
         {
