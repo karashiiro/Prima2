450eae842968f0f8c667b9d2b78bb33b8c6fbe85
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-21 16:42:51 -0700

    Remove drsupgraderoles

--- Prima.Stable/Modules/OwnerModule.cs
+++ Prima.Stable/Modules/OwnerModule.cs
@@ -23,50 +23,5 @@ namespace Prima.Stable.Modules
             await Db.RemoveBrokenUsers();
             await ReplyAsync("Done!");
         }
-
-        [Command("drsupgraderoles", RunMode = RunMode.Async)]
-        [RequireContext(ContextType.Guild)]
-        public async Task DRSUpgradeRoles([Remainder] string roleName)
-        {
-            var role = Context.Guild.Roles.FirstOrDefault(r => r.Name == roleName);
-            if (role == null)
-            {
-                await ReplyAsync("No role by that name exists!");
-                return;
-            }
-
-            if (!DelubrumProgressionRoles.Roles.ContainsKey(role.Id))
-            {
-                await ReplyAsync("Not a valid DRS role!");
-                return;
-            }
-
-            var contingentRoles = DelubrumProgressionRoles.GetContingentRoles(role.Id)
-                .Select(cr => Context.Guild.GetRole(cr))
-                .ToList();
-
-            await Context.Guild.DownloadUsersAsync();
-            var applicableUsers = Context.Guild.Users
-                .Where(u => u.HasRole(role))
-                .ToList();
-
-            var left = applicableUsers.Count;
-            var pogRess = await ReplyAsync($"Downloaded full user list, upgrading {left} users...");
-            foreach (var user in applicableUsers)
-            {
-                foreach (var cr in contingentRoles)
-                {
-                    if (!user.HasRole(cr))
-                    {
-                        await user.AddRoleAsync(cr);
-                    }
-                }
-
-                left--;
-                await pogRess.ModifyAsync(props => props.Content = $"Downloaded full user list, upgrading {left} users...");
-            }
-
-            await pogRess.ModifyAsync(props => props.Content = "Roles updated!");
-        }
     }
 }
