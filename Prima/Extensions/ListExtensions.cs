9b9c2b076f82b58c84ac41e621d7f291c55735fa
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-05-24 10:03:59 -0700

    Undo that because it deadlocks?

--- Prima/Extensions/ListExtensions.cs
+++ Prima/Extensions/ListExtensions.cs
@@ -34,20 +34,17 @@ namespace Prima.Extensions
         [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
         public static IList<T> RemoveAll<T>(this IList<T> list, Predicate<T> predicate, bool overload = false)
         {
-            lock (list)
+            var ret = new List<T>();
+            for (var i = 0; i < list.Count; i++)
             {
-                var ret = new List<T>();
-                for (var i = 0; i < list.Count; i++)
+                if (predicate(list[i]))
                 {
-                    if (predicate(list[i]))
-                    {
-                        ret.Add(list[i]);
-                        list.Remove(list[i]);
-                        i--;
-                    }
+                    ret.Add(list[i]);
+                    list.Remove(list[i]);
+                    i--;
                 }
-                return ret;
             }
+            return ret;
         }
     }
 }
