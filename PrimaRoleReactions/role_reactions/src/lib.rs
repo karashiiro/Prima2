3e32d4f94740fa2295d3f40812ab2160fbf20370
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-06-23 12:34:48 -0700

    Implement get command for reactions

--- PrimaRoleReactions/role_reactions/src/lib.rs
+++ PrimaRoleReactions/role_reactions/src/lib.rs
@@ -17,10 +17,27 @@ impl Handler {
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "~ping" {
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
-                println!("Error sending message: {:?}", why);
+    //noinspection SpellCheckingInspection
+    async fn message(&self, ctx: Context, message: Message) {
+        // Commands are only applicable to guilds
+        if message.guild_id.is_none() {
+            return;
+        }
+
+        let guild_id = *message.guild_id.unwrap().as_u64();
+
+        if message.content == "~rolereactions" {
+            match self.db.get_role_reactions(guild_id).await {
+                Ok(role_reactions) => {
+                    for _ in role_reactions {
+                        if let Err(why) = message.channel_id.say(&ctx.http, "A role reaction.").await {
+                            println!("Error sending message: {:?}", why);
+                        }
+                    }
+                }
+                Err(error) => {
+                    println!("Failed to fetch role reactions for guild: {:?}", error);
+                }
             }
         }
     }
