65d705616c5f2494f2b8420c83f3651ef5600ec4
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-06-22 17:22:00 -0700

    Create role reactions project

---
+++ PrimaRoleReactions/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(non_snake_case)] // This disables this lint for the entire crate - most code should stay in the library, not here
+use std::env;
+
+use serenity::prelude::*;
+
+use role_reactions::Handler;
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("PRIMA_BOT_TOKEN").expect("Expected a token in the environment");
+
+    let mut client = Client::builder(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
