c278e337806cd89a2e599d619586664eab1957c0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-16 14:00:28 -0700

    Add comment about member join gateway event

--- Prima.Stable/Handlers/WelcomeCard.cs
+++ Prima.Stable/Handlers/WelcomeCard.cs
@@ -13,6 +13,9 @@ namespace Prima.Stable.Handlers
     {
         public static async Task Handler(ITemplateProvider templates, SocketMessage message)
         {
+            // This is being done this way rather than with the MEMBER_ADD gateway event because
+            // that event just isn't being received. All intents are enabled, and the bot is not
+            // in more than 100 servers. In another server, it worked fine for some reason.
             if (message.Channel is not IGuildChannel channel || channel.Guild.Id != SpecialGuilds.CrystalExploratoryMissions) return;
             if (channel.Id != channel.Guild.SystemChannelId) return;
 
