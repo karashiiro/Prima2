22d056c8934431326c532ced1cb6fe3edc2306e8
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-17 13:31:38 -0700

    Add fallback for Kupo Bot in welcome card

--- Prima.Stable/Handlers/WelcomeCard.cs
+++ Prima.Stable/Handlers/WelcomeCard.cs
@@ -1,4 +1,6 @@
-﻿using Discord;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Discord;
 using Discord.WebSocket;
 using Prima.DiscordNet.Extensions;
 using Prima.Resources;
@@ -16,9 +18,29 @@ namespace Prima.Stable.Handlers
             // that event just isn't being received. All intents are enabled, and the bot is not
             // in more than 100 servers. In another server, it worked fine for some reason.
             if (message.Channel is not IGuildChannel channel || channel.Guild.Id != SpecialGuilds.CrystalExploratoryMissions) return;
-            if (channel.Id != channel.Guild.SystemChannelId || message.Source != MessageSource.System) return;
 
-            var user = message.Author;
+            IUser user = null;
+            if (await channel.Guild.GetChannelAsync(857729033562226748) != null) // Kupo bot join channel, posts 100% of the time
+            {
+                if (channel.Id == 857729033562226748 && message.Author.Id == 107256979105267712)
+                {
+                    var embed = message.Embeds.FirstOrDefault();
+                    var footer = embed?.Footer?.Text;
+                    if (string.IsNullOrEmpty(footer)) return;
+
+                    var userIdMatch = new Regex(@"\d+").Match(footer);
+                    if (!userIdMatch.Success) return;
+
+                    if (!ulong.TryParse(userIdMatch.Value, out var userId)) return;
+
+                    user = await client.GetUserAsync(userId);
+                }
+            }
+            else if (channel.Id == channel.Guild.SystemChannelId && message.Source == MessageSource.System) // Fallback to system channel
+            {
+                user = message.Author;
+            }
+
             if (user == null) return;
 
             await user.SendMessageAsync(embed: templates.Execute("cemjoin.md", new
