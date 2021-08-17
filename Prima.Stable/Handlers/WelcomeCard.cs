eb706feb55a35d980843a1da04a46097366ccf17
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-08-17 13:37:50 -0700

    Improve comments in welcome card code

--- Prima.Stable/Handlers/WelcomeCard.cs
+++ Prima.Stable/Handlers/WelcomeCard.cs
@@ -20,17 +20,20 @@ namespace Prima.Stable.Handlers
             if (message.Channel is not IGuildChannel channel || channel.Guild.Id != SpecialGuilds.CrystalExploratoryMissions) return;
 
             IUser user = null;
-            if (await channel.Guild.GetChannelAsync(857729033562226748) != null) // Kupo bot join channel, posts 100% of the time
+            if (await channel.Guild.GetChannelAsync(857729033562226748) != null) // Kupo Bot join channel, posts 100% of the time
             {
                 if (channel.Id == 857729033562226748 && message.Author.Id == 107256979105267712)
                 {
+                    // Try to get the message embed footer text
                     var embed = message.Embeds.FirstOrDefault();
                     var footer = embed?.Footer?.Text;
                     if (string.IsNullOrEmpty(footer)) return;
 
+                    // Don't message people when they leave the server
                     var leaving = embed.Description.Contains("left");
                     if (leaving) return;
 
+                    // Footer is formatted as: "User ID: 12345678901234567890"
                     var userIdMatch = new Regex(@"\d+").Match(footer);
                     if (!userIdMatch.Success) return;
 
@@ -39,7 +42,7 @@ namespace Prima.Stable.Handlers
                     user = await client.GetUserAsync(userId);
                 }
             }
-            else if (channel.Id == channel.Guild.SystemChannelId && message.Source == MessageSource.System) // Fallback to system channel
+            else if (channel.Id == channel.Guild.SystemChannelId && message.Source == MessageSource.System) // Fallback to system channel, sometimes doesn't post
             {
                 user = message.Author;
             }
