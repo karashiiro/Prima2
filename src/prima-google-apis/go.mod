cae4b76f36dae789c6f3923eceefb576fed0a032
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-02 09:45:55 -0800

    Copy config repo and GAPI repo

---
+++ src/prima-google-apis/go.mod
@@ -0,0 +1,10 @@
+module github.com/karashiiro/prima-google-apis
+
+go 1.15
+
+require (
+	github.com/gofiber/fiber/v2 v2.5.0
+	github.com/olebedev/config v0.0.0-20190528211619-364964f3a8e4 // indirect
+	golang.org/x/oauth2 v0.0.0-20210201163806-010130855d6c
+	google.golang.org/api v0.39.0
+)
---
+++ src/prima-google-apis/google_api_client.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/sheets/v4"
+)
+
+func newGoogleAPI(tokenPath, credentialPath string) *http.Client {
+	config := getGoogleAPIConfig(credentialPath)
+	client := getGoogleAPIClient(config, tokenPath)
+	return client
+}
+
+func getGoogleAPIClient(config *oauth2.Config, tokenPath string) *http.Client {
+	tok, err := tokenFromFile(tokenPath)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokenPath, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+func getGoogleAPIConfig(credentialPath string) *oauth2.Config {
+	b, err := ioutil.ReadFile(credentialPath)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, calendar.CalendarEventsScope, sheets.SpreadsheetsScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+
+	return config
+}
+
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	log.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	log.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
