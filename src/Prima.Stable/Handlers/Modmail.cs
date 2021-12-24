a7fe9087383855174095b9c6263f80497013b8c7
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-12-24 00:28:03 -0800

    Begin work on new modmail system

---
+++ src/Prima.Stable/Handlers/Modmail.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Serilog;
+using System.Threading.Tasks;
+
+namespace Prima.Stable.Handlers
+{
+    public static class Modmail
+    {
+        public static async Task Handler(SocketMessageComponent component)
+        {
+            if (component.Data.CustomId != "cem-modmail") return;
+            if (component.Message.Channel is not ITextChannel channel) return;
+
+            var member = await channel.Guild.GetUserAsync(component.User.Id);
+            var threadName = string.IsNullOrEmpty(member.Nickname) ? member.ToString() : member.Nickname;
+            var thread = await channel.CreateThreadAsync(threadName, ThreadType.PrivateThread);
+            await thread.AddUserAsync(member);
+
+            Log.Information("Created thread \"{ThreadName}\" for user \"{User}\".", threadName, member.ToString());
+        }
+    }
+}
