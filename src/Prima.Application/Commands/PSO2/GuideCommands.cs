322cd845db01ba06a58d6a332e8e85d221825fa6
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-06-30 12:45:11 -0700

    Migrate PSO2 guides module

---
+++ src/Prima.Application/Commands/PSO2/GuideCommands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Prima.DiscordNet;
+using Prima.DiscordNet.Attributes;
+using Prima.Resources;
+
+namespace Prima.Application.Commands.PSO2;
+
+[Name("PSO2 Guides")]
+public class GuideCommands : ModuleBase<SocketCommandContext>
+{
+    private readonly HttpClient _http;
+
+    public GuideCommands(HttpClient http)
+    {
+        _http = http;
+    }
+
+    [Command("aeriomats")]
+    [Description("Shows the Aerio materials route.")]
+    [RestrictFromGuilds(SpecialGuilds.CrystalExploratoryMissions)]
+    public Task AerioMaterials()
+        => DiscordUtilities.PostImage(_http, Context, "https://i.imgur.com/I8J001K.png");
+}
