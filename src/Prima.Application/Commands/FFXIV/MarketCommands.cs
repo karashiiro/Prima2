5fe2428ce80aeed081f78787eb7d456f3a4d356d
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-09-11 15:19:22 -0700

    Move market and roll commands to new application

---
+++ src/Prima.Application/Commands/FFXIV/MarketCommands.cs
@@ -0,0 +1,75 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json.Linq;
+using Prima.DiscordNet.Attributes;
+using Prima.Game.FFXIV.XIVAPI;
+
+namespace Prima.Application.Commands.FFXIV;
+
+[Name("FFXIV Market")]
+public class MarketCommands : ModuleBase<SocketCommandContext>
+{
+    private readonly HttpClient _http;
+    private readonly XIVAPIClient _xivapi;
+
+    public MarketCommands(HttpClient http, XIVAPIClient xivapi)
+    {
+        _http = http;
+        _xivapi = xivapi;
+    }
+
+    [Command("market", RunMode = RunMode.Async)]
+    [Description("[FFXIV] Look up market data for an item. Usage: `~market <item name> <world>`")]
+    public async Task MarketAsync(params string[] args)
+    {
+        if (args.Length < 2)
+        {
+            await ReplyAsync(
+                $"{Context.User.Mention}, please provide an item name in your command, followed by the World or DC name to query.");
+            return;
+        }
+
+        var itemName = string.Join(' ', args[0..^2]);
+        var worldName = args[^1];
+        worldName = char.ToUpper(worldName[0]) + worldName[1..];
+
+        var searchResults = await _xivapi.SearchItem(itemName);
+        if (searchResults.Count == 0)
+        {
+            await ReplyAsync($"No results found for \"{itemName}\", are you sure you spelled the item name correctly?");
+            return;
+        }
+
+        var searchData = searchResults
+            .Where(result => string.Equals(result.Name, itemName, StringComparison.CurrentCultureIgnoreCase))
+            .ToList();
+        var item = !searchData.Any() ? searchResults.First() : searchData.First();
+
+        var itemId = item.Id;
+        itemName = item.Name;
+
+        var uniResponse = await _http.GetAsync(new Uri($"https://universalis.app/api/{worldName}/{itemId}"));
+        var dataObject = await uniResponse.Content.ReadAsStringAsync();
+        var listings = JObject.Parse(dataObject)["Results"].ToObject<IList<UniversalisListing>>();
+        var trimmedListings = listings.Take(Math.Min(10, listings.Count())).ToList();
+
+        await ReplyAsync($"__{listings.Count} results for {worldName} (Showing up to 10):__\n" +
+                         trimmedListings.Select(listing => listing.Quantity + " **" + itemName + "** for " +
+                                                           listing.PricePerUnit + " Gil " +
+                                                           (!string.IsNullOrEmpty(listing.WorldName)
+                                                               ? "on " +
+                                                                 listing.WorldName + " "
+                                                               : "") + (listing.Quantity > 1
+                                                               ? $" (For a total of {listing.Total} Gil)"
+                                                               : "")));
+    }
+
+    public class UniversalisListing
+    {
+        public bool Hq { get; set; }
+        public int PricePerUnit { get; set; }
+        public int Quantity { get; set; }
+        public string RetainerName { get; set; }
+        public int Total { get; set; }
+        public string WorldName { get; set; }
+    }
+}
---
+++ src/Prima.Application/Commands/Random/ExtraCommands.cs
@@ -0,0 +1,71 @@
+﻿using Discord.Commands;
+using Prima.DiscordNet.Attributes;
+using Prima.Game.FFXIV.XIVAPI;
+using Prima.Services;
+
+namespace Prima.Application.Commands.Random;
+
+[Name("Extra")]
+public class ExtraCommands : ModuleBase<SocketCommandContext>
+{
+    public IDbService Db { get; set; }
+    public HttpClient Http { get; set; }
+    public XIVAPIClient Xivapi { get; set; }
+
+    [Command("roll", RunMode = RunMode.Async)]
+    [Description("T̵̪͖̎̈̍ḛ̷̤͑̚ș̴̔͑̾ͅͅt̸͔͜͝ ̶̡̨̪͌̉͠ỷ̵̺̕o̴̞̍ū̴͚̣̤r̵͚͎͔͘ ̴̨̬̿ḷ̷͖̀̚u̴͖̲͌́c̴̲̣͙͑̈͝k̸͍͖̿̆̓!̶̢̅̀")]
+    public async Task RollAsync([Remainder] string args = "")
+    {
+        var res = (int)Math.Floor(new System.Random().NextDouble() * 4);
+        switch (res)
+        {
+            case 0:
+                await ReplyAsync(
+                    $"BINGO! You matched {(int)Math.Floor(new System.Random().NextDouble() * 11) + 1} lines! Congratulations!");
+                break;
+            case 1:
+                var opt = (int)Math.Floor(new System.Random().NextDouble() * 2598960) + 1;
+                switch (opt)
+                {
+                    case <= 4:
+                        await ReplyAsync("JACK**P**O*T!* Roya**l flush!** You __won__ [%#*(!@] credits*!*");
+                        break;
+                    case > 4 and <= 40:
+                        await ReplyAsync(
+                            "Straight flush! You won [TypeError: Cannot read property 'MUNZ' of undefined] credits!");
+                        break;
+                    case > 40 and <= 664:
+                        await ReplyAsync(
+                            "Four of a kind! You won [TypeError: Cannot read property 'thinking' of undefined] credits!");
+                        break;
+                    case > 664 and <= 4408:
+                        await ReplyAsync(
+                            "Full house! You won [TypeError: Cannot read property '<:GWgoaThinken:582982105282379797>' of undefined] credits!");
+                        break;
+                    case > 4408 and <= 9516:
+                        await ReplyAsync("Flush! You won -1 credits!");
+                        break;
+                    case > 9516 and <= 19716:
+                        await ReplyAsync("Straight! You won -20 credits!");
+                        break;
+                    case > 19716 and <= 74628:
+                        await ReplyAsync("Two pairs...? You won -500 credits!");
+                        break;
+                    case > 198180 and <= 1296420:
+                        await ReplyAsync("One pair. You won -2500 credits.");
+                        break;
+                    default:
+                        await ReplyAsync("No pairs. You won -10000 credits.");
+                        break;
+                }
+                break;
+            case 2:
+                await ReplyAsync(
+                    $"Critical hit! You dealt {(int)Math.Floor(new System.Random().NextDouble() * 9999) + 9999} damage!");
+                break;
+            case 3:
+                await ReplyAsync("You took 9999999 points of damage. You lost the game.");
+                break;
+        }
+    }
+}
