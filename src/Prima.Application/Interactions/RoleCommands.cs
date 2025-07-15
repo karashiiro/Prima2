e003edbb17cc0b4f68220a10cdc8c731bf229be0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2025-07-14 20:03:18 -0700

    Fix bugs and add role restore command

--- src/Prima.Application/Interactions/RoleCommands.cs
+++ src/Prima.Application/Interactions/RoleCommands.cs
@@ -3,6 +3,7 @@ using Discord.Interactions;
 using Microsoft.Extensions.Logging;
 using Prima.DiscordNet;
 using Prima.DiscordNet.Attributes;
+using Prima.Resources;
 using Prima.Services;
 
 namespace Prima.Application.Interactions;
@@ -25,13 +26,20 @@ public class RoleCommands : InteractionModuleBase<SocketInteractionContext>
         var guild = Context.Guild;
         if (guild == null)
         {
-            await ReplyAsync("This command can only be used in a guild.");
+            await RespondAsync("This command can only be used in a guild.");
             return;
         }
 
-        _logger.LogInformation("Setting main vanity role for {User} to {Role}", Context.User.Username, role.Name);
-
         var user = await Context.Client.Rest.GetGuildUserAsync(guild.Id, Context.User.Id);
+        
+        var allGuildVanityRoles = SpecialGuildVanityRoles.GetRoles(user.GuildId);
+        if (!allGuildVanityRoles.Contains(role.Id))
+        {
+            await RespondAsync("That role is not a valid vanity role.", ephemeral: true);
+            return;       
+        }
+
+        _logger.LogInformation("Setting main vanity role for {User} to {Role}", Context.User.Username, role.Name);
 
         // Register any untracked vanity roles
         await user.UpdateVanityRoles(_db, _logger);
@@ -62,6 +70,46 @@ public class RoleCommands : InteractionModuleBase<SocketInteractionContext>
             await user.RemoveRoleAsync(roleId);
         }
         await user.AddRoleAsync(role.Id);
+
+        await RespondAsync($"Updated main vanity role to {role.Name}.", ephemeral: true);
+    }
+
+    [SlashCommand("restore-vanity-roles", "Restores your vanity roles to their default state.")]
+    public async Task RestoreVanityRoles()
+    {
+        var guild = Context.Guild;
+        if (guild == null)
+        {
+            await RespondAsync("This command can only be used in a guild.");
+            return;
+        }
+
+        _logger.LogInformation("Restoring vanity roles for {User}", Context.User.Username);
+
+        var user = await Context.Client.Rest.GetGuildUserAsync(guild.Id, Context.User.Id);
+
+        // Register any untracked vanity roles
+        await user.UpdateVanityRoles(_db, _logger);
+        
+        var dbUser = await _db.GetUserByDiscordId(user.Id);
+        if (dbUser == null)
+        {
+            await RespondAsync("You are not currently registered. Please register yourself with `~iam`.",
+                ephemeral: true);
+            return;
+        }
+
+        // Get the vanity roles the user has
+        var roleIds = dbUser.GetVanityRoles(guild.Id);
+        _logger.LogInformation("Found {RoleCount} vanity roles for {User}", roleIds.Count, Context.User.Username);
+
+        // Restore all vanity roles
+        foreach (var roleId in roleIds)
+        {
+            await user.AddRoleAsync(roleId);
+        }
+
+        await RespondAsync("Restored all vanity roles.", ephemeral: true);
     }
 
     [SlashCommand("list-vanity-roles", "Check your vanity roles on this server.")]
@@ -70,7 +118,7 @@ public class RoleCommands : InteractionModuleBase<SocketInteractionContext>
         var guild = Context.Guild;
         if (guild == null)
         {
-            await ReplyAsync("This command can only be used in a guild.");
+            await RespondAsync("This command can only be used in a guild.");
             return;
         }
 
